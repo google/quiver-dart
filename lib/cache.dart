e9d8d4ca9b8f25b287100291dd560c6cf148eccd
Justin Fagnani <justin@fagnani.com>
2013-08-21 20:04:44 -0700

    Initial cache.dart checkin

---
+++ lib/cache.dart
@@ -0,0 +1,90 @@
+// Copyright 2013 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+library quiver.cache;
+
+import 'dart:async';
+import 'dart:collection';
+
+/**
+ * A function that produces a value for [key], for when a [Cache] needs to
+ * populate an entry.
+ *
+ * The loader function should either return a value synchronously or a [Future]
+ * which completes with the value asynchronously.
+ */
+typedef dynamic Loader<K, V>(K key);
+
+/**
+ * A semi-persistent mapping of keys to values. All access to a Cache is
+ * asynchronous because many implementations will store their entries in
+ * remote systems, isolates, or otherwise have to do async IO to read and write.
+ */
+abstract class Cache<K, V> {
+
+  /**
+   * Returns the value associated with [key].
+   */
+  Future<V> get(K key, {Loader<K, V> ifAbsent});
+
+  /**
+   * Sets the value associated with [key].
+   */
+  Future set(K key, V value);
+
+  /**
+   * Removes the value associated with [key].
+   */
+  Future invalidate(K key);
+}
+
+/**
+ * A [Cache] that's backed by a [Map].
+ */
+class MapCache<K, V> implements Cache<K, V> {
+
+  final Map<K, V> _map;
+
+  /**
+   * Creates a new [LocalCache], optionally using [map] as the backing [Map].
+   */
+  MapCache({Map<K, V> map})
+      : _map = map != null ? map : new HashMap<K, V>();
+
+  Future<V> get(K key, {Loader<K, V> ifAbsent}) {
+    if (!_map.containsKey(key) && ifAbsent != null) {
+      var valOrFuture = ifAbsent(key);
+      if (valOrFuture is Future) {
+        return valOrFuture.then((v) {
+          _map[key] = v;
+          return v;
+        });
+      } else {
+        _map[key] = valOrFuture;
+        return new Future.value(valOrFuture);
+      }
+    }
+    return new Future.value(_map[key]);
+  }
+
+  Future set(K key, V value) {
+    _map[key] = value;
+    return new Future.value();
+  }
+
+  Future invalidate(K key) {
+    _map.remove(key);
+    return new Future.value();
+  }
+}
