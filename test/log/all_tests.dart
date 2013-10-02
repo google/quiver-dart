d639adb2c31a6488944e229eef527593882855a2
Adam Bender <bendera@google.com>
2013-10-02 10:06:06 -0700

    initial import of log

--- test/log/all_tests.dart
+++ test/log/all_tests.dart
@@ -12,28 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-library quiver.all_tests;
+library quiver.log.all_tests;
 
-import 'async/all_tests.dart' as async;
-import 'cache/map_cache_test.dart' as cache;
-import 'collection/all_tests.dart' as collection;
-import 'io_test.dart' as io;
-import 'iterables/all_tests.dart' as iterables;
-import 'log/all_tests.dart' as log;
-import 'mirrors_test.dart' as mirrors;
-import 'pattern/all_tests.dart' as pattern;
-import 'strings_test.dart' as strings;
-import 'time/all_tests.dart' as time;
 
 main() {
-  async.main();
-  cache.main();
-  collection.main();
-  io.main();
-  iterables.main();
-  log.main();
-  mirrors.main();
-  pattern.main();
-  strings.main();
-  time.main();
 }
