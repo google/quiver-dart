27d24f51d40670cc7c31b9ac90f89d0815a750e9
Sean Eagan <seaneagan1@gmail.com>
2014-02-14 14:49:56 -0600

    Add duration unit constants to quiver.time

---
+++ lib/src/time/duration_unit_constants.dart
@@ -0,0 +1,23 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+part of quiver.time;
+
+const Duration aMicrosecond = const Duration(microseconds: 1);
+const Duration aMillisecond = const Duration(milliseconds: 1);
+const Duration aSecond = const Duration(seconds: 1);
+const Duration aMinute = const Duration(minutes: 1);
+const Duration anHour = const Duration(hours: 1);
+const Duration aDay = const Duration(days: 1);
+const Duration aWeek = const Duration(days: 7);
