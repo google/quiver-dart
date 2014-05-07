9e478d48ccd9f6d879696289426dcba46bdcc8f5
John McDole <jtmcdole@gmail.com>
2014-05-06 17:22:29 -0700

    Reviewer notes

--- lib/src/time/watch_clock.dart
+++ lib/src/time/watch_clock.dart
@@ -1,4 +1,4 @@
-// Copyright 2013 Google Inc. All Rights Reserved.
+// Copyright 2014 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ part of quiver.time;
  *
  * Example:
  *
- *     wallClock(aMinute).listen((d) => print(d));
+ *     watchClock(aMinute).listen((d) => print(d));
  *
  * Prints the following stream of events till the stream is canceled:
  *     2014-05-04 14:06:00.000
@@ -37,15 +37,18 @@ Stream<DateTime> watchClock(Duration interval, {Clock clock: SYSTEM_CLOCK}) {
     timer.cancel();
   });
 
+  _startTimer(DateTime now, tick) {
+    var delay = intervalMs - (now.millisecondsSinceEpoch % intervalMs);
+    return new Timer(new Duration(milliseconds: delay), tick);
+  }
+
   tick() {
+    if (controller.isClosed) return;
     DateTime now = clock.now();
     controller.add(now);
-    if (controller.isClosed) return;
-    var delay = intervalMs - (now.millisecondsSinceEpoch % intervalMs);
-    timer = new Timer(new Duration(milliseconds: delay), tick);
+    timer = _startTimer(now, tick);
   };
-  var delay = intervalMs - (clock.now().millisecondsSinceEpoch % intervalMs);
-  timer = new Timer(new Duration(milliseconds: delay), tick);
+  timer = _startTimer(clock.now(), tick);
   return controller.stream;
 }
 
