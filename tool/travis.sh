a3d2fe3136d80a261fd47edda1f759f5dd78f866
Chris Bracken <chris@bracken.jp>
2015-02-03 19:49:25 -0800

    Make --fatal-warnings env-based for Travis.

--- tool/travis.sh
+++ tool/travis.sh
@@ -15,16 +15,10 @@ echo "Executing pub get..."
 pub get
 
 # Verify that the libraries are error and warning-free.
-echo "Analyzing top-level libraries..."
+echo "Running dartanalyzer..."
 libs=$(find lib -maxdepth 1 -type f -name '*.dart')
-dartanalyzer --fatal-warnings $libs
-
-echo "Analyzing testing libraries..."
 testing_libs=$(find lib/testing -maxdepth 1 -type f -name '*.dart')
-dartanalyzer --fatal-warnings $testing_libs
-
-echo "Analyzing tests..."
-dartanalyzer --fatal-warnings test/all_tests.dart
+dartanalyzer $DARTANALYZER_FLAGS $libs $testing_libs test/all_tests.dart
 
 # Run the tests.
 echo "Running tests..."
