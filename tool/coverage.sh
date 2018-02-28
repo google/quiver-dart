18f731c9e5b1828145d82472a679a80e84d02ce0
Matan Lurey <matanl@google.com>
2018-02-28 12:41:51 -0800

    Add build_runner-based travis/DDC integration.

--- tool/coverage.sh
+++ tool/coverage.sh
@@ -7,32 +7,8 @@
 # Fast fail the script on failures.
 set -e
 
-# Run pub get to fetch dependencies.
-echo "Running pub get..."
-pub get
-
-# Verify that the libraries are error and warning-free.
-echo "Running dartanalyzer..."
-libs=$(find lib -maxdepth 1 -type f -name '*.dart')
-testing_libs=$(find lib/testing -maxdepth 1 -type f -name '*.dart')
-dartanalyzer $DARTANALYZER_FLAGS $libs $testing_libs test/all_tests.dart
-
-# Verify that dartfmt has been run.
-if [[ "$TRAVIS_DART_VERSION" == "stable" ]]; then
-  # Only test on stable to avoid CI failure due to diffs between stable and dev.
-  echo "Checking dartfmt..."
-  if [[ $(dartfmt -n --set-exit-if-changed lib/ test/) ]]; then
-    echo "Failed dartfmt check: run dartfmt -w lib/ test/"
-    exit 1
-  fi
-fi
-
-# Run the tests.
-echo "Running tests..."
-pub run test:test --reporter expanded
-
 # Gather and send coverage data.
-if [ "$REPO_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
+if [ "$REPO_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "dev" ]; then
   OBS_PORT=9292
   echo "Collecting coverage on port $OBS_PORT..."
 
@@ -57,4 +33,6 @@ if [ "$REPO_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
     --out=var/lcov.info \
     --packages=.packages \
     --report-on=lib
+
+  coveralls-lcov var/lcov.info
 fi
