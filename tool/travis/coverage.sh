9d29153eb7aa1d24c6f503c409c814dd54e5ae08
Chris Bracken <chris@bracken.jp>
2018-03-24 14:41:27 -0700

    Add debug logging for Travis coverage (#444)

--- tool/travis/coverage.sh
+++ tool/travis/coverage.sh
@@ -13,6 +13,7 @@ if [ "$REPO_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "dev" ]; then
   echo "Collecting coverage on port $OBS_PORT..."
 
   # Start tests in one VM.
+  echo "Starting tests..."
   dart \
     --enable-vm-service=$OBS_PORT \
     --pause-isolates-on-exit \
@@ -20,6 +21,7 @@ if [ "$REPO_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "dev" ]; then
 
   # Run the coverage collector to generate the JSON coverage report.
   pub global activate coverage
+  echo "Collecting coverage..."
   pub global run coverage:collect_coverage \
     --port=$OBS_PORT \
     --out=var/coverage.json \
@@ -34,5 +36,6 @@ if [ "$REPO_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "dev" ]; then
     --packages=.packages \
     --report-on=lib
 
+  echo "Uploading to Coveralls..."
   coveralls-lcov --repo-token="$REPO_TOKEN" var/lcov.info
 fi
