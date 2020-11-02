edd9ea9ddc0f2b42fda66d0a35488985d04bf3c2
Chris Bracken <chris@bracken.jp>
2020-11-02 09:38:34 -0800

    Migrate async/future_stream to non-null by default (#657)

--- test/async/future_stream_test.dart
+++ test/async/future_stream_test.dart
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// @dart = 2.9
-
 library quiver.async.future_stream_test;
 
 import 'dart:async';
