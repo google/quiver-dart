f051d998b929a578a3f2ac01bc3648acc5a1d874
Chris Bracken <chris@bracken.jp>
2020-11-03 10:29:42 -0800

    Remove remaining null-safety opt-out comments (#663)

--- test/collection/all_tests.dart
+++ test/collection/all_tests.dart
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// @dart = 2.9
-
 library quiver.collection.all_tests;
 
 import 'bimap_test.dart' as bimap;
