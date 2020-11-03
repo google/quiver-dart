dd883f3e1baab70ebaac3cbe8b6a23be4bebd124
Chris Bracken <chris@bracken.jp>
2020-11-02 18:08:22 -0800

    Migrate collection/lru_map to non-null by default (#651)

--- test/collection/lru_map_test.dart
+++ test/collection/lru_map_test.dart
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// @dart = 2.9
-
 library quiver.collection.lru_map_test;
 
 import 'package:quiver/src/collection/lru_map.dart';
@@ -21,8 +19,8 @@ import 'package:test/test.dart';
 
 void main() {
   group('LruMap', () {
-    /// A map that will be initialize by individual tests.
-    /*late*/ LruMap<String, String> lruMap;
+    /// A map that will be initialized by individual tests.
+    late LruMap<String, String> lruMap;
 
     test('the length property reflects how many keys are in the map', () {
       lruMap = LruMap();
@@ -319,7 +317,7 @@ void main() {
   });
 
   group('LruMap builds an informative string representation', () {
-    /*late*/ LruMap<String, dynamic> lruMap;
+    late LruMap<String, dynamic> lruMap;
 
     setUp(() {
       lruMap = LruMap();
